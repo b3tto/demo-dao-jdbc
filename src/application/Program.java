cb815f99c6e831ab064b183bbe86f4a371c90d7f
Alberto Medeiros <b3tomedeiros@gmail.com>
2025-09-07 16:45:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Date;
+
 import modules.entities.Department;
 
 public class Program {
@@ -7,7 +9,10 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department dep = new Department(1, "Books");
-		System.out.println(dep);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
+		
+		System.out.println(seller);
 
 	}
 
